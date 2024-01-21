46992d89dcf1678b88d670686b9f0bb2e95e18f5
Porsha <78366933+quistty@users.noreply.github.com>
2024-01-20 22:58:09 -0500

    wrong branch but created exercise command

--- cpt.java
+++ cpt.java
@@ -87,7 +87,7 @@ public static boolean whyisthisbroken = false;
     }
 
     public static void blackjack() {
-
+        
     }
 
     public static void slots() {
@@ -942,7 +942,44 @@ public static boolean whyisthisbroken = false;
         }
         return schoolTitle;
     }
-
+    public static void exercise() {
+        System.out.println("Pick how you want to exercise");
+        System.out.println("1. Reach your PR (lift weights)");
+        System.out.println("2. Run");
+        System.out.println("3. Diet");
+        int bossmanInput = in.nextInt();
+        double random;
+        int randomInt;
+        // gives health based on the method of study
+        switch (bossmanInput) {
+            case 1:
+                random = (5 - 1 + 1) * Math.random() + 1; // grants a random amount of intelligence
+                randomInt = (int) random;
+                healthStats += randomInt;
+                System.out.println("You surpassed your PR! You gained +" + randomInt + "% health.");
+                System.out.println("Your current health is: " + healthStats);
+                happinessStats -= 3;
+                break;
+            case 2:
+                random = (7 - 1 + 1) * Math.random() + 1; // grants a random amount of intelligence
+                randomInt = (int) random;
+                healthStats += randomInt;
+                System.out.println("You hit a new 5k run time! You gained: +" + randomInt + "% health.");
+                System.out.println("Your current health is: " + healthStats);
+                happinessStats -= 3;
+                break;
+            case 3:
+                random = (10 - 1 + 1) * Math.random() + 1; // grants a random amount of intelligence
+                randomInt = (int) random;
+                healthStats += randomInt;
+                System.out.println("Your lost 5 pounds from your diet! Your gained: +" + randomInt + "% health");
+                System.out.println("Your new health is: " + healthStats);
+                happinessStats -= 5;
+                break;
+        }
+        System.out.println("Don't exercise too much or you'll loose happiness");
+        
+    }
     public static void quitJob() {
         if (jobStatus == false) { // err msg
             System.out.println("You don't have a job! You can't quit something you don't have.");
@@ -1193,6 +1230,9 @@ public static boolean whyisthisbroken = false;
                     return;
                 case "quit": //lets user quit job
                     quitJob();
+                    return;
+                case "exercise": //lets user exercise
+                    exercise();
                     return;
                 default: // err msg
                     System.out.println("Unknown command, make sure it exists and try again!");
